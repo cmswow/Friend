242d96f6ee6868d3c1419337ecc3eece3cbe731e
cmswow <127907534+cmswow@users.noreply.github.com>
2023-08-06 14:58:07 +0900

    20230806

--- src/main/java/com/example/friend/controller/FriendController.java
+++ src/main/java/com/example/friend/controller/FriendController.java
@@ -1,6 +1,7 @@
 package com.example.friend.controller;
 
 
+import com.example.friend.domain.Friend;
 import com.example.friend.dto.FriendDTO;
 import com.example.friend.dto.PageRequestDTO;
 import com.example.friend.dto.PageResponseDTO;
@@ -9,8 +10,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping
@@ -26,5 +32,34 @@ public class FriendController {
             log.info(responseDTO);
             model.addAttribute("responseDTO",responseDTO);
         }
+
+        @GetMapping("/register")
+        public void registerGET() {
+
+        }
+
+        @PostMapping("/register")
+        public String register(@Valid FriendDTO friendDTO, BindingResult bindingResult, RedirectAttributes rttr) {
+
+            if (bindingResult.hasErrors()) {
+                rttr.addFlashAttribute("errors", bindingResult.getAllErrors());
+                return "redirect:/register";
+            }
+
+        Long fno = friendService.register(friendDTO);
+            rttr.addFlashAttribute("result",fno);
+
+
+            return "redirect:/home";
+        }
+
+        @GetMapping({"/read","/modify"})
+        public void read(Long fno,PageRequestDTO pageRequestDTO,Model model){
+
+            FriendDTO friendDTO = friendService.readOne(fno);
+            model.addAttribute("dto",friendDTO);
+        }
+
+
 
 }
